97e0d90cdb65c169417942ff9ff9f78bae8c0a97
voron434 <070809010@mail.ru>
2020-05-11 17:43:16 +0300

    Update code after first review

--- server.py
+++ server.py
@@ -10,13 +10,15 @@ from aiohttp import web
 
 CHUNK_SIZE = 10**6  # ~100KB per iteration
 
+logger = logging.getLogger("app")
+
 
 async def archivate(request):
-    archive_hash = request.match_info.get('archive_hash')
+    archive_hash = request.match_info['archive_hash']
 
     filepath_to_be_zipped = os.path.join(request.app.source_path, archive_hash)
     if not os.path.exists(filepath_to_be_zipped):
-        raise aiohttp.web.HTTPFound()
+        raise aiohttp.web.HTTPNotFound()
 
     response = web.StreamResponse()
     response.headers['Content-Disposition'] = f'attachment; filename="{archive_hash}.zip"'
@@ -31,20 +33,18 @@ async def archivate(request):
     try:
         while True:
             line = await zip_process.stdout.read(CHUNK_SIZE)
-            if line == b'':
+            if not line:
                 break
             await response.write(line)
             if request.app.enable_throttling:
                 await asyncio.sleep(1)
-        if request.app.enable_logging:
-            app.logger.info(f"Load sucess for request on {request.raw_path}")
+        logger.info(f"Load sucess for request on {request.raw_path}")
     except asyncio.CancelledError:
-        if request.app.enable_logging:
-            app.logger.warning(f"Load interrupted for request on {request.raw_path}")
+        logger.warning(f"Load interrupted for request on {request.raw_path}")
         zip_process.kill()
         await zip_process.communicate()
-    finally:
-        return response
+        raise
+    return response
 
 
 async def handle_index_page(request):
@@ -53,27 +53,29 @@ async def handle_index_page(request):
     return web.Response(text=index_contents, content_type='text/html')
 
 
-def configure_app_with_argparse(app):
+def create_argparser():
     parser = argparse.ArgumentParser(description='Cloud storage web-server')
-    parser.add_argument('-p', '--source_path', help='Catalog where files are stored')
-    parser.add_argument('-l', '--enable_logging', action='store_true', help='Enadles logging')
+    parser.add_argument('-p', '--source_path', default='test_photos', help='Catalog where files are stored')
+    parser.add_argument('-v', '--verbose', action='count', default=0, help='Enadles logging. Set -v for debug, -vvvvv for critical.')
     parser.add_argument('-t', '--enable_throttling', action='store_true', help='Add server-side pauses for debugging')
-    
-    args = parser.parse_args()
-    
-    app.enable_throttling = args.enable_throttling
-    app.enable_logging = args.enable_logging
-    app.source_path = args.source_path or 'test_photos'
-
-    logger = logging.getLogger("app")
-    logger.setLevel(logging.INFO)
-    app.logger = logger
-    return app
+    return parser
 
 
 if __name__ == '__main__':
+    parser = create_argparser()
+    args = parser.parse_args()
+    
+    formatter = logging.Formatter('%(filename)s[LINE:%(lineno)d]# %(levelname)-8s [%(asctime)s]  %(message)s')
+    handler = logging.StreamHandler()
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+    logger.setLevel(args.verbose * 10)
+
     app = web.Application()
-    app = configure_app_with_argparse(app)
+
+    app.enable_throttling = args.enable_throttling
+    app.source_path = args.source_path
+    
     app.add_routes([
         web.get('/', handle_index_page),
         web.get('/archive/{archive_hash}/', archivate),
